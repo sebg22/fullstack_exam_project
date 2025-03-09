bbda46aeb4f83e22b05cfe46f4fc4044a9ea7112
Julie <julieeriksen09@hotmail.com>
2025-03-09 16:55:08 +0100

    Created the skeleton of the component

--- rawg-client/src/App.tsx
+++ rawg-client/src/App.tsx
@@ -2,24 +2,26 @@ import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import CryptoTable from "./components/CryptoTable";
+import LargeGuide from "./components/LargeGuide";
 
 function App() {
   return (
     <>
-    <Grid templateAreas={{ base: `"header" "main"`, lg: `"header header" "aside main"` }}>
-      <GridItem pl="2" area={"header"}>
-        <NavBar />
-      </GridItem>
-      <Show above="lg">
-        <GridItem w="150px" pl="2" bg="gray.900" area={"aside"}>
-          aside
+      <Grid templateAreas={{ base: `"header" "main"`, lg: `"header header" "aside main"` }}>
+        <GridItem pl="2" area={"header"}>
+          <NavBar />
         </GridItem>
-      </Show>
-      <GridItem pl="8" pr="8" area={"main"}>
-        <GameGrid></GameGrid>
-      </GridItem>
-    </Grid>
-    <CryptoTable />
+        <Show above="lg">
+          <GridItem w="150px" pl="2" bg="gray.900" area={"aside"}>
+            aside
+          </GridItem>
+        </Show>
+        <GridItem pl="8" pr="8" area={"main"}>
+          <GameGrid></GameGrid>
+        </GridItem>
+      </Grid>
+      <LargeGuide label="Beginner's Guide" title="What is Bitcoin" description="Bitcoin is the world's first widely adopted cryptocurrency — it allows for secure and seamless peer-to-peer transactions on the internet." />
+      <CryptoTable />
     </>
   );
 }
---
+++ rawg-client/src/components/LargeGuide.tsx
@@ -0,0 +1,24 @@
+import { Link } from "@chakra-ui/react";
+import logo from "../assets/logo.png";
+
+//props which is sent from the parent component
+interface props {
+  label: string;
+  title: string;
+  description: string;
+}
+
+const LargeGuide = ({ label, title, description }: props) => {
+  return (
+    <>
+      <Link display="block" _hover={{ textDecoration: "none" }} href="/">
+        <img src={logo} alt="Logo" />
+        <span>{label}</span>
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </Link>
+    </>
+  );
+};
+
+export default LargeGuide;
