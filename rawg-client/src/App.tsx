520508cebf331606bc7887e8c609bcbd8aecc737
Oliver Jenbo <cph-oj92@cphbusiness.dk>
2025-03-09 14:19:36 +0100

    added Login to navBar and routing to it

--- rawg-client/src/App.tsx
+++ rawg-client/src/App.tsx
@@ -1,26 +1,35 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import CryptoTable from "./components/CryptoTable";
+import Login from "./login";
 
 function App() {
   return (
-    <>
-    <Grid templateAreas={{ base: `"header" "main"`, lg: `"header header" "aside main"` }}>
-      <GridItem pl="2" area={"header"}>
-        <NavBar />
-      </GridItem>
-      <Show above="lg">
-        <GridItem w="150px" pl="2" bg="gray.900" area={"aside"}>
-          aside
+    <Router>
+      <Grid 
+        templateAreas={{ base: `"header" "main"`, lg: `"header header" "aside main"` }}
+      >
+        <GridItem pl="2" area={"header"}>
+          <NavBar />
         </GridItem>
-      </Show>
-      <GridItem pl="8" pr="8" area={"main"}>
-        <GameGrid></GameGrid>
-      </GridItem>
-    </Grid>
-    <CryptoTable />
-    </>
+
+        <Show above="lg">
+          <GridItem w="150px" pl="2" area={"aside"}>
+            aside
+          </GridItem>
+        </Show>
+
+        <GridItem pl="8" pr="8" area={"main"}>
+          <Routes>
+            <Route path="/" element={<GameGrid />} /> {/* Add a route to the GameGrid component */}
+            <Route path="/login" element={<Login />} /> {/* Add a route to the Login component */}
+          </Routes>
+        </GridItem>
+      </Grid>
+      <CryptoTable />
+    </Router>
   );
 }
 
--- rawg-client/src/components/NavBar.tsx
+++ rawg-client/src/components/NavBar.tsx
@@ -1,12 +1,19 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image, Button, Spacer } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 import logo from "../assets/logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
+  const navigate = useNavigate(); // React Router hook for navigation
+
   return (
-    <HStack>
-      <Image src={logo} boxSize="60px"></Image>
-      <ColorModeSwitch></ColorModeSwitch>
+    <HStack width="100%" padding="10px">
+      <Image src={logo} boxSize="60px" />
+      <ColorModeSwitch />
+      <Spacer /> {/* Pushes the login button to the right */}
+      <Button colorScheme="blue" onClick={() => navigate("/login")}> {/* Navigate to /login */}
+        Login
+      </Button>
     </HStack>
   );
 };
