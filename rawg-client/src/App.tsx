28c9b8ee39790a090e0cd77581f21962994b14ea
Julie <julieeriksen09@hotmail.com>
2025-03-12 16:15:16 +0100

    added image

--- rawg-client/src/App.tsx
+++ rawg-client/src/App.tsx
@@ -1,8 +1,9 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Flex, Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import CryptoTable from "./components/CryptoTable";
 import LargeGuide from "./components/LargeGuide";
+import image1 from "./assets/Learn_Illustration1.webp";
 
 function App() {
   return (
@@ -20,7 +21,11 @@ function App() {
           <GameGrid></GameGrid>
         </GridItem>
       </Grid>
-      <LargeGuide label="Beginner's Guide" title="What is Bitcoin" description="Bitcoin is the world's first widely adopted cryptocurrency — it allows for secure and seamless peer-to-peer transactions on the internet." />
+      <Flex>
+        <LargeGuide img={image1} label="Beginner's Guide" title="What is Bitcoin" description="Bitcoin is the world's first widely adopted cryptocurrency — it allows for secure and seamless peer-to-peer transactions on the internet." />
+        <LargeGuide img={image1} label="Beginner's Guide" title="Guide to DeFi tokens and altcoins" description="From Aave to Zcash, decide what to trade with our beginner's guide" />
+      </Flex>
+
       <CryptoTable />
     </>
   );
--- rawg-client/src/components/LargeGuide.tsx
+++ rawg-client/src/components/LargeGuide.tsx
@@ -1,20 +1,24 @@
-import { Link } from "@chakra-ui/react";
-import logo from "../assets/logo.png";
+import { Box, Img, Link } from "@chakra-ui/react";
 
 //props which is sent from the parent component
 interface props {
+  img: string;
   label: string;
   title: string;
   description: string;
 }
 
-const LargeGuide = ({ label, title, description }: props) => {
+const LargeGuide = ({ img, label, title, description }: props) => {
   return (
     <>
-      <Link bg="blue" display="block" _hover={{ textDecoration: "none" }} href="/">
-        <img src={logo} alt="Logo" />
-        <span>{label}</span>
-        <h3>{title}</h3>
+      <Link w="100%" m="20px" display="block" _hover={{ textDecoration: "none" }} href="/">
+        <Img w="100%" mb="10px" h="300px" src={img} alt="" />
+        <Box>
+          <span>{label}</span>
+        </Box>
+        <Box my="5px" fontWeight={600} fontSize={28}>
+          <h3>{title}</h3>
+        </Box>
         <p>{description}</p>
       </Link>
     </>
