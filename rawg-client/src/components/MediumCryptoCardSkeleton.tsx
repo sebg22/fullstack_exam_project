b74b209cc8da244bb2a3f7d0925bd2410840434a
Julie <julieeriksen09@hotmail.com>
2025-03-15 13:27:08 +0100

    Removed comments

--- rawg-client/src/components/MediumCryptoCardSkeleton.tsx
+++ rawg-client/src/components/MediumCryptoCardSkeleton.tsx
@@ -5,9 +5,9 @@ const MediumCryptoCardSkeleton = () => {
     <SimpleGrid columns={{ base: 1, md: 2 }} spacing={4}>
       <Card p={4} shadow="md" borderWidth="2px" borderRadius="25px" width="90%">
         <CardHeader>
-          <Skeleton height="24px" width="50px" mb={2} /> {/* Symbol Skeleton */}
-          <Skeleton height="28px" width="80px" mb={2} /> {/* Price Skeleton */}
-          <Skeleton height="24px" width="100px" /> {/* 24h Change Skeleton */}
+          <Skeleton height="24px" width="50px" mb={2} />
+          <Skeleton height="28px" width="80px" mb={2} />
+          <Skeleton height="24px" width="100px" />
         </CardHeader>
       </Card>
     </SimpleGrid>
