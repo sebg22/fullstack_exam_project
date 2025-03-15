952a26f71c285ab5182e60568377896bfbcab8b1
sebg22 <sebmp48@gmail.com>
2025-03-15 14:48:50 +0100

    Making aside

--- rawg-client/src/App.tsx
+++ rawg-client/src/App.tsx
@@ -1,7 +1,12 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+
+type Filters = {
+  // Define the properties of Filters here
+};
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import CryptoTable from "./components/CryptoTable";
+import SideMenu from "./components/SideMenu";
 
 function App() {
   return (
@@ -12,7 +17,9 @@ function App() {
         </GridItem>
         <Show above="lg">
           <GridItem w="150px" pl="2" bg="gray.900" area={"aside"}>
-            aside
+            <SideMenu setFilter={function (filter: string): void {
+              throw new Error("Function not implemented.");
+            } } />
           </GridItem>
         </Show>
         <GridItem pl="8" pr="8" area={"main"}>
---
+++ rawg-client/src/components/SideMenu.tsx
@@ -0,0 +1,104 @@
+import { Box, Collapse, Divider, Flex, GridItem, Menu } from "@chakra-ui/react";
+import { useState } from "react";
+
+const SideMenu = ({ setFilter }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [showTopOptions, setShowTopOptions] = useState(false); 
+  const [showPriceOptions, setShowPriceOptions] = useState(false);
+
+  return (
+    <GridItem w="150px" pl="2" area={"aside"}>
+      <Menu>
+        <nav>
+          <Box mt={10}>
+            <h2 style={{ paddingBottom: "10px", fontWeight: "bold" }}>Cryptocurrencies</h2>
+            <ul>
+              {/* "Top" Category with Show More */}
+              <li
+                style={{ display: "inline-flex", listStyleType: "none", marginBottom: "4px", cursor: "pointer" }}
+                onClick={() => setShowTopOptions(!showTopOptions)}
+              >
+                <Flex align="center" gap={2}>
+                  🔝 Top Coins {showTopOptions ? "▲" : "▼"}
+                </Flex>
+              </li>
+
+              {/* Expandable "Top" Filters */}
+              <Collapse in={showTopOptions} animateOpacity>
+                <ul>
+                  <li style={{ display: "inline-flex", marginBottom: "4px", cursor: "pointer", paddingLeft: "15px" }} onClick={() => setFilter("top10")}>
+                    Top 10 Coins
+                  </li>
+                  <li style={{ display: "inline-flex", marginBottom: "4px", cursor: "pointer", paddingLeft: "15px" }} onClick={() => setFilter("top50")}>
+                    Top 50 Coins
+                  </li>
+                  <li style={{ display: "inline-flex", marginBottom: "4px", cursor: "pointer", paddingLeft: "15px" }} onClick={() => setFilter("top100")}>
+                    Top 100 Coins
+                  </li>
+                </ul>
+              </Collapse>
+
+              {/* Price Range with Show More */}
+              <Divider />
+              <li
+                style={{ display: "inline-flex", listStyleType: "none", marginBottom: "4px", cursor: "pointer" }}
+                onClick={() => setShowPriceOptions(!showPriceOptions)}
+              >
+                <Flex align="center" gap={2}>
+                  💲 Price Range {showPriceOptions ? "▲" : "▼"}
+                </Flex>
+              </li>
+
+              {/* Expandable Price Range Filters */}
+              <Collapse in={showPriceOptions} animateOpacity>
+                <ul>
+                  <li style={{ display: "inline-flex", marginBottom: "4px", cursor: "pointer", paddingLeft: "15px" }} onClick={() => setFilter("under1")}>
+                    Under $1
+                  </li>
+                  <li style={{ display: "inline-flex", marginBottom: "4px", cursor: "pointer", paddingLeft: "15px" }} onClick={() => setFilter("1to100")}>
+                    $1 - $100
+                  </li>
+                  <li style={{ display: "inline-flex", marginBottom: "4px", cursor: "pointer", paddingLeft: "15px" }} onClick={() => setFilter("above100")}>
+                    Above $100
+                  </li>
+                </ul>
+              </Collapse>
+
+              {/* 24h Price Change */}
+              <Divider />
+              <li style={{ display: "inline-flex", marginBottom: "4px", cursor: "pointer" }} onClick={() => setFilter("gainers")}>
+                📈 Only Gainers (24h)
+              </li>
+              <li style={{ display: "inline-flex", marginBottom: "4px", cursor: "pointer" }} onClick={() => setFilter("losers")}>
+                📉 Only Losers (24h)
+              </li>
+
+              {/* Stablecoins Filter */}
+              <Divider />
+              <li style={{ display: "inline-flex", marginBottom: "4px", cursor: "pointer" }} onClick={() => setFilter("stablecoins")}>
+                🏦 Show Only Stablecoins
+              </li>
+
+              {/* All-Time High (ATH) Distance */}
+              <Divider />
+              <li style={{ display: "inline-flex", marginBottom: "4px", cursor: "pointer" }} onClick={() => setFilter("ath")}>
+                🚀 Remove Coins 80% Below ATH
+              </li>
+
+              {/* Coin Age Filter */}
+              <Divider />
+              <li style={{ display: "inline-flex", marginBottom: "4px", cursor: "pointer" }} onClick={() => setFilter("newCoins")}>
+                🆕 Show New Coins (Last 6 Months)
+              </li>
+              <li style={{ display: "inline-flex", marginBottom: "4px", cursor: "pointer" }} onClick={() => setFilter("oldCoins")}>
+                🏛️ Show Old Coins (Over 5 Years)
+              </li>
+            </ul>
+          </Box>
+        </nav>
+      </Menu>
+    </GridItem>
+  );
+};
+
+export default SideMenu;
