2f88d0d753083b3e387ba9d42ccf5aec481c8b75
Julie <julieeriksen09@hotmail.com>
2025-05-31 14:03:55 +0200

    updated

--- crypto-client/src/pages/CryptoCurrencies.tsx
+++ crypto-client/src/pages/CryptoCurrencies.tsx
@@ -5,27 +5,26 @@ import useCrypto from "../hooks/useCrypto";
 
 export default function CryptoCurrencies() {
   const { cryptos, loading, error } = useCrypto(); // Get data using our custom hook
-  return (
-    <>
-      <Grid
-        templateAreas={{
-          base: `"header" "main"`,
-          lg: `"header header" "aside main"`,
-        }}>
-        <Show above="lg">
-          <GridItem area="aside" w="150px" pl="2">
-            <SideMenu
-              setFilter={function (filter: string): void {
-                throw new Error("Function not implemented.");
-              }}
-            />
-          </GridItem>
-        </Show>
 
-        <GridItem area="main">
-          <CryptoTable cryptos={cryptos} loading={loading} error={error} />
+  return (
+    <Grid
+      templateAreas={{
+        base: `"header" "main"`,
+        lg: `"header header" "aside main"`,
+      }}>
+      <Show above="lg">
+        <GridItem area="aside" w="100%" pl="2" pr="4">
+          <SideMenu
+            setFilter={function (filter: string): void {
+              throw new Error("Function not implemented.");
+            }}
+          />
         </GridItem>
-      </Grid>
-    </>
+      </Show>
+
+      <GridItem area="main">
+        <CryptoTable cryptos={cryptos} loading={loading} error={error} />
+      </GridItem>
+    </Grid>
   );
 }
