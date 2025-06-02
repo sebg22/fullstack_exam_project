e9f392cd683e76b8a9f037a48a678f95152e3218
Jenbo <oliverjenbo677@gmail.com>
2025-06-02 16:31:58 +0200

    Pr ready

--- crypto-client/src/routes.tsx
+++ crypto-client/src/routes.tsx
@@ -8,6 +8,7 @@ import Login from "./pages/Login";
 import Coin from "./pages/Coin";
 import Signup from "./pages/Signup";
 import CryptoCurrencies from "./pages/CryptoCurrencies";
+import Learn from "./pages/Learn";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,7 @@ const router = createBrowserRouter([
       { path: "cryptocurrencies", element: <CryptoCurrencies /> }, // GET “/crypto-currencies”
       { path: "signup", element: <Signup /> },               // GET “/login”
       { path: "coin/:id", element: <Coin /> },             // GET “/coin/:id”
+      { path: "learn", element: <Learn /> },             // GET “/learn”
 
     ],
   },
