9538c9c6c282ab8ffd9084b91a6b5b4a80587fb2
Julie <julieeriksen09@hotmail.com>
2025-06-07 16:56:34 +0200

    Updated twitter icon to work with react/typescript

--- crypto-client/src/components/Footer.tsx
+++ crypto-client/src/components/Footer.tsx
@@ -1,17 +1,7 @@
 import React from "react";
-import {
-  Box,
-  Grid,
-  GridItem,
-  Stack,
-  Text,
-  Link,
-  IconButton,
-} from "@chakra-ui/react";
-import { FaFacebookF, FaDiscord } from "react-icons/fa";
+import { Box, Grid, GridItem, Stack, Text, Link, IconButton } from "@chakra-ui/react";
+import { FaFacebookF, FaDiscord, FaTwitter } from "react-icons/fa";
 import CoinvaultLogo from "./CoinVaultLogo";
-import { FaXTwitter } from "react-icons/fa6";
-
 
 const Footer: React.FC = () => {
   return (
@@ -34,27 +24,17 @@ const Footer: React.FC = () => {
         }}
         gap={{ base: 6, md: 8 }}
         w="100%"
-        px={{ base: 4, md: 4 }}
-      >
-        <GridItem
-          area="logo"
-          display={{ base: "block", md: "none" }}
-          textAlign="center">
+        px={{ base: 4, md: 4 }}>
+        <GridItem area="logo" display={{ base: "block", md: "none" }} textAlign="center">
           <CoinvaultLogo boxSize="150px" mx="auto" />
         </GridItem>
 
-        <GridItem
-          area="address"
-          mt={{ base: -8, md: 0 }}
-          textAlign={{ base: "center", md: "left" }}
-          alignSelf={{ base: "auto", md: "center" }}
-        >
-          <Stack
-            spacing={2}
-            align={{ base: "center", md: "flex-start" }}
-          >
-            <Text fontWeight="bold" fontSize={{base: "md", md: "lg" }}>Address:</Text>
-            <Text fontSize={{base: "md", md: "lg" }} color="footerSubTextColor" lineHeight="1.4">
+        <GridItem area="address" mt={{ base: -8, md: 0 }} textAlign={{ base: "center", md: "left" }} alignSelf={{ base: "auto", md: "center" }}>
+          <Stack spacing={2} align={{ base: "center", md: "flex-start" }}>
+            <Text fontWeight="bold" fontSize={{ base: "md", md: "lg" }}>
+              Address:
+            </Text>
+            <Text fontSize={{ base: "md", md: "lg" }} color="footerSubTextColor" lineHeight="1.4">
               1234 Crypto Lane,
               <br />
               Blocktown, Web3 9000
@@ -62,17 +42,12 @@ const Footer: React.FC = () => {
           </Stack>
         </GridItem>
 
-        <GridItem
-          area="contact"
-          textAlign={{ base: "center", md: "right" }}
-          alignSelf={{ base: "auto", md: "center" }}
-        >
-          <Stack
-            spacing={2}
-            align={{ base: "center", md: "flex-end" }}
-          >
-            <Text fontWeight="bold" fontSize={{base: "md", md: "lg" }}>Contact:</Text>
-            <Text fontSize={{base: "md", md: "lg" }} color="footerSubTextColor" lineHeight="1.4">
+        <GridItem area="contact" textAlign={{ base: "center", md: "right" }} alignSelf={{ base: "auto", md: "center" }}>
+          <Stack spacing={2} align={{ base: "center", md: "flex-end" }}>
+            <Text fontWeight="bold" fontSize={{ base: "md", md: "lg" }}>
+              Contact:
+            </Text>
+            <Text fontSize={{ base: "md", md: "lg" }} color="footerSubTextColor" lineHeight="1.4">
               support@coinvault.fake
               <br />
               +45 123 456 78
@@ -80,99 +55,41 @@ const Footer: React.FC = () => {
           </Stack>
         </GridItem>
 
-        <GridItem
-          area="icons"
-          display={{ base: "block", md: "none" }}
-          textAlign="center"
-        >
+        <GridItem area="icons" display={{ base: "block", md: "none" }} textAlign="center">
           <Stack direction="row" justify="center" spacing={6}>
             <Link href="https://facebook.com" isExternal>
-              <IconButton
-                aria-label="Facebook"
-                icon={<FaFacebookF />}
-                variant="ghost"
-                size="lg"
-                _hover= {{bg: "footerHoverColor "}}
-              />
+              <IconButton aria-label="Facebook" icon={<FaFacebookF />} variant="ghost" size="lg" _hover={{ bg: "footerHoverColor " }} />
             </Link>
             <Link href="https://x.com" isExternal>
-              <IconButton
-                aria-label="Twitter"
-                icon={<FaXTwitter />}
-                variant="ghost"
-                size="lg"
-                _hover={{bg: "footerHoverColor "}}
-              />
+              <IconButton aria-label="Twitter" icon={<FaTwitter />} variant="ghost" size="lg" _hover={{ bg: "footerHoverColor " }} />
             </Link>
             <Link href="https://discord.com" isExternal>
-              <IconButton
-                aria-label="Discord"
-                icon={<FaDiscord />}
-                variant="ghost"
-                size="lg"
-                _hover={{bg: "footerHoverColor "}}
-              />
+              <IconButton aria-label="Discord" icon={<FaDiscord />} variant="ghost" size="lg" _hover={{ bg: "footerHoverColor " }} />
             </Link>
           </Stack>
         </GridItem>
 
-
-        <GridItem
-          area="center"
-          display={{ base: "none", md: "flex" }}
-          flexDir="column"
-          alignItems="center"
-          justifyContent="center"
-          rowGap={6}
-        >
+        <GridItem area="center" display={{ base: "none", md: "flex" }} flexDir="column" alignItems="center" justifyContent="center" rowGap={6}>
           {/* Logo for size md+ */}
-          <CoinvaultLogo boxSize="150px"  />
-          
+          <CoinvaultLogo boxSize="150px" />
 
           {/* Social icons for size md+ */}
           <Stack direction="row" spacing={6}>
             <Link href="https://facebook.com" isExternal>
-              <IconButton
-                aria-label="Facebook"
-                icon={<FaFacebookF />}
-                variant="ghost"
-                size="lg"
-                _hover={{bg: "footerHoverColor "}}
-              />
+              <IconButton aria-label="Facebook" icon={<FaFacebookF />} variant="ghost" size="lg" _hover={{ bg: "footerHoverColor " }} />
             </Link>
             <Link href="https://x.com" isExternal>
-              <IconButton
-                aria-label="X / Twitter"
-                icon={<FaXTwitter />}
-                variant="ghost"
-                size="lg"
-                _hover={{bg: "footerHoverColor "}}
-              />
+              <IconButton aria-label="X / Twitter" icon={<FaTwitter />} variant="ghost" size="lg" _hover={{ bg: "footerHoverColor " }} />
             </Link>
             <Link href="https://discord.com" isExternal>
-              <IconButton
-                aria-label="Discord"
-                icon={<FaDiscord />}
-                variant="ghost"
-                size="lg"
-                _hover={{bg: "footerHoverColor "}}
-              />
+              <IconButton aria-label="Discord" icon={<FaDiscord />} variant="ghost" size="lg" _hover={{ bg: "footerHoverColor " }} />
             </Link>
           </Stack>
         </GridItem>
 
         {/* COPYRIGHT (spans all 3 columns on md, appears last in mobile) */}
-        <GridItem
-          area="copyright"
-          colSpan={{ base: 1, md: 3 }}
-          textAlign="center"
-        >
-          <Box
-            borderTop="1px solid"
-            borderColor="divider"
-            pt={4}
-            mt={{ base: 6, md: 8 }}
-          >
+        <GridItem area="copyright" colSpan={{ base: 1, md: 3 }} textAlign="center">
+          <Box borderTop="1px solid" borderColor="divider" pt={4} mt={{ base: 6, md: 8 }}>
             <Text fontSize="sm" color="footerSubTextColor">
               © {new Date().getFullYear()} CoinVault
             </Text>
